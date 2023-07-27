c98e3e9bc7eb38aa26e6a2a2b798a59c9d034e5a
Hrishi2520 <hrishikeshshedge0@gmail.com>
2023-07-27 11:50:59 +0530

    Autowiring with @Qualifier Annotation

--- springcore/src/main/java/com/springcore/autowire/annotation/Employee.java
+++ springcore/src/main/java/com/springcore/autowire/annotation/Employee.java
@@ -1,9 +1,11 @@
 package com.springcore.autowire.annotation;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 
 public class Employee {
     @Autowired
+    @Qualifier("address2")
     private Address address;     //for Property injection
     public Employee() {
     }
