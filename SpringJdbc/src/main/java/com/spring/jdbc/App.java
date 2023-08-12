9bf768c02390f352eefd12856604c136662974dd
Hrishi2520 <hrishikeshshedge0@gmail.com>
2023-08-12 11:36:10 +0530

    Spring JDBC Configuration without XML

--- SpringJdbc/src/main/java/com/spring/jdbc/App.java
+++ SpringJdbc/src/main/java/com/spring/jdbc/App.java
@@ -1,9 +1,11 @@
 package com.spring.jdbc;
 
+import com.spring.jdbc.config.JdbcConfig;
 import com.spring.jdbc.dao.StudentDao;
 import com.spring.jdbc.dao.StudentDaoImpl;
 import com.spring.jdbc.entity.Student;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.jdbc.core.JdbcTemplate;
 
@@ -19,7 +21,7 @@ public class App
     {
         System.out.println( "My Program Started......!" );
 //        spring jdbc=> JdbcTemplate
-        ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
+        ApplicationContext context = new AnnotationConfigApplicationContext(JdbcConfig.class);
         JdbcTemplate template = context.getBean("jdbcTemplate", JdbcTemplate.class);
 
         StudentDao studentDao = context.getBean("studentDao", StudentDao.class);
