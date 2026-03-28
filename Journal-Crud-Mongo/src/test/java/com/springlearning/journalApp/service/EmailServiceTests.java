49e399e44783d055698af201455615296280ec71
Hrishi2520 <hrishikeshshedge0@gmail.com>
2026-03-28 23:49:01 +0530

    Java Email Sending...

--- Journal-Crud-Mongo/src/test/java/com/springlearning/journalApp/service/EmailServiceTests.java
+++ Journal-Crud-Mongo/src/test/java/com/springlearning/journalApp/service/EmailServiceTests.java
@@ -1,7 +1,17 @@
 package com.springlearning.journalApp.service;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-public class EmailServiceTests {
+class EmailServiceTests {
+
+    @Autowired
+    private EmailService emailService;
+
+    @Test
+    void testSendMail() {
+        emailService.sendEmail("hrishikeshshedge1@gmail.com", "Testing java mail sender", "Hello...");
+    }
 }
