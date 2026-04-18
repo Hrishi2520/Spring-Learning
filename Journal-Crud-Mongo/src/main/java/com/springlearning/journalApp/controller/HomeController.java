dc4ca7db7255e8a5241f05a29724127cd31a5e3f
Hrishi2520 <hrishikeshshedge0@gmail.com>
2026-04-19 00:10:32 +0530

    added operations in Home  Controller

--- Journal-Crud-Mongo/src/main/java/com/springlearning/journalApp/controller/HomeController.java
+++ Journal-Crud-Mongo/src/main/java/com/springlearning/journalApp/controller/HomeController.java
@@ -1,11 +1,13 @@
 package com.springlearning.journalApp.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 
+    @Operation(summary = "Home API")
     @GetMapping("/")
     public String healthCheck() {
         return "Home";
